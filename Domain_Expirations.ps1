850444ce28074169d4b26a26af94f95815c6b26e
jonathanrebellion <jonathanrebellion@gmail.com>
2014-12-30 20:56:36 -0600

    Initial Commit

---
+++ Domain_Expirations.ps1
@@ -0,0 +1,12 @@
+﻿$domainarray = Get-Content ( read-host -Prompt 'Where is your text file with domain names (full path including file name)?')
+foreach  ($domain in $domainarray) {
+    if ($domain -like "*.org"){
+     $info=.\WhoisCL.exe -r $domain |select-string -pattern "Expiry" -AllMatches
+     $domain
+     $info}
+    Else{
+    $info=.\WhoisCL.exe -r $domain |select-string -pattern "Expiration" -AllMatches
+    $domain
+    $info
+    }
+}
